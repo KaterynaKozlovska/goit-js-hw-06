9445c3833635656cef409fd794bdebdd254752e9
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-02-03 23:04:28 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const listOfIngredients = document.querySelector('#ingredients');
+
+const newListOfLi = ingredients.map(ingredient => {
+  const newLi = document.createElement('li');
+  newLi.textContent = ingredient.valueOf(ingredients);
+  newLi.className = 'item';
+
+  return newLi;
+});
+
+listOfIngredients.append(...newListOfLi);
