03e7969a00ec23c6362123e5dda9742b5d64bc01
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-02-06 23:03:16 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,22 +1,20 @@
 const input = document.querySelector('#validation-input');
-const inputTextLength = input.dataset.length;
 
-const inputText = () => {
+input.addEventListener('blur', event => {
   if (input.value.length === Number(input.dataset.length)) {
-    input.setAttribute('class', 'valid');
-  } else {
-    input.setAttribute('class', 'invalid');
+    input.classList.remove('invalid');
+    return input.classList.add('valid');
   }
-};
+  input.classList.remove('valid');
+  return input.classList.add('invalid');
+});
 
-input.addEventListener('input', inputText);
-
-// const textInput = document.querySelector('#validation-input');
-
-// textInput.addEventListener('blur', event => {
-//   if (textInput.value.length === Number(textInput.dataset.length)) {
+// const inputText = () => {
+//   if (input.value.length === Number(input.dataset.length)) {
 //     input.setAttribute('class', 'valid');
 //   } else {
 //     input.setAttribute('class', 'invalid');
 //   }
-// });
+// };
+
+// input.addEventListener('input', inputText);
