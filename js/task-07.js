b729082fd25d7ba73fc4d7ea690dee8172115f3a
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-02-06 21:37:02 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,5 @@ const input = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
 
 input.addEventListener('input', event => {
-  console.dir(`${event.currentTarget.value}px`);
-  console.dir(text.style.fontSize);
+  text.style.fontSize = `${event.currentTarget.value}px`;
 });
